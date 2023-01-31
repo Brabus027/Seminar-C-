457d6470bca1429d6523f6622a423925af90a01d
Brabus027 <113928520+Brabus027@users.noreply.github.com>
2023-01-31 15:17:56 +0300

    Past 41 and 43

---
+++ Seminar6_dz/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Задача 41: Пользователь вводит с клавиатуры M чисел.
+//   Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+/*Console.Write("Введите ваши числа через пробел: ");
+int[] numbers = GetArrayFromString(Console.ReadLine());
+
+Console.WriteLine($"Числа больше нуля: {ResultArray(numbers)}");
+
+int[] GetArrayFromString(string stringArray) 
+{                                             
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int ResultArray(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (numbers[i] > 0) result++;
+    }
+    return result;
+}
+*/
+
+// Задача 43: Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в 
+// промежутке от 0 до 100, а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему)
+
+
+
